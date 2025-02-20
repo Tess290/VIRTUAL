e461b30291caa2e5fd510dcf30262b47e18c5163
Tess290 <emmatessi9@gmail.com>
2025-02-20 01:00:55 +0100

    projet

---
+++ index.js
@@ -0,0 +1,31 @@
+// Condition
+let x = 6;
+if (x>0){
+    console.log("Le résultat est positif");
+    
+}   else {
+    console.log("Le resultat est négatif");
+
+}
+
+// if/else ==>if
+//  switch on les utilise rarement
+
+// Boucle while
+let i = 0;
+
+while (i < 30){
+    console.log(i);
+    i += 4;
+}
+
+// Fonction
+function somme(x, y) {
+    let s =x+y;
+    return s;
+}
+console.log(somme(3, 34));
+function carre (c){
+    let car = c*c
+    return car;
+}
